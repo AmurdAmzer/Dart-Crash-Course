b853ff90a0458208c09ee62adfb33124890f29bf
AmurdAmzer <amurdamzer@gmail.com>
2024-05-28 09:52:23 -0400

    commit changes

---
+++ varaibles_and_data_types/bin/varaibles_and_data_types.dart
@@ -0,0 +1,5 @@
+import 'package:varaibles_and_data_types/varaibles_and_data_types.dart' as varaibles_and_data_types;
+
+void main(List<String> arguments) {
+  print('Hello world: ${varaibles_and_data_types.calculate()}!');
+}
---
+++ varaibles_and_data_types/test/varaibles_and_data_types_test.dart
@@ -0,0 +1,8 @@
+import 'package:varaibles_and_data_types/varaibles_and_data_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
